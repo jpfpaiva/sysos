3b77e887cafff935dee2f7ecc121f07eca973980
Daniel Sava <danielsavas@gmail.com>
2015-04-26 01:22:40 -0300

    Alteração Classe Pessoa. Adicionado campo de Data de Nascimento.

--- dominio/src/main/java/entidade/Pessoa.java
+++ dominio/src/main/java/entidade/Pessoa.java
@@ -1,8 +1,20 @@
 package entidade;
 
+import java.util.Date;
+
 public class Pessoa {
 	
 	private String nome;
+	
+	private Date dataNascimento;
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 
 	public String getNome() {
 		return nome;
